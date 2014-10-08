13bc2d08532c43c746ae743b79dc7af94a43ad4d
unknown <dvolykhin@spb-dvolykhin-PC.global.axiomsl.com>
2014-10-08 11:38:41 +0400

    items list

--- background.js
+++ background.js
@@ -1,17 +1,42 @@
+var storage = chrome.storage.local,
+
+
+addStart = function (value) {
+    storage.get('time', function (item) {
+      item = item.time || [];
+
+      item.push('Start ' + value);
+
+      storage.set({time: item});
+    });
+ };
+
+ addEnd = function (value) {
+    storage.get('time', function (item) {
+      item = item.time || [];
+
+      item.push('End ' + value);
+
+      storage.set({time: item});
+    });
+ };
+
 window.onload = function() {
 
 	var bkg = chrome.extension.getBackgroundPage();
 
 	chrome.windows.onCreated.addListener(function () {
 		// Save it using the Chrome extension storage API.
-        chrome.storage.local.set({'start': moment().format('MMMM Do YYYY, h:mm:ss')});
+        // chrome.storage.local.set({'start': moment().format('MMMM Do YYYY, h:mm:ss')});
+     addStart(moment().format('MMMM Do YYYY, h:mm:ss'));   
 
 	});
 
 
 	chrome.windows.onRemoved.addListener(function () {
 		// Save it using the Chrome extension storage API.
-        chrome.storage.local.set({'end' : moment().format('MMMM Do YYYY, h:mm:ss')});
+        // chrome.storage.local.set({'end' : moment().format('MMMM Do YYYY, h:mm:ss')});
+     addEnd(moment().format('MMMM Do YYYY, h:mm:ss'));      
 
 	});
 
--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 function buildPopupDom(divName, data) {
   var ul = document.getElementById(divName);
 
-  // for (var i = 0, ie = data.length; i < ie; ++i) {
+   for (var i = 0, ie = data.time.length; i < ie; ++i) {
     var p = document.createElement('p');
-    p.appendChild(document.createTextNode(data.start + '-' + data.end ));
+    p.appendChild(document.createTextNode(data.time[i]));
 
     var li = document.createElement('li');
     li.appendChild(p);
 
     ul.appendChild(li);
-  // }
+  }
 }
 
 
 document.addEventListener('DOMContentLoaded', function () {
   
-   chrome.storage.local.get('start', function(items) {
+   chrome.storage.local.get('time', function(items) {
           // Notify that we saved.
         	console.log('Get items');
         	console.log(items);
